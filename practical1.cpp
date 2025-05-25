8217e789c77c6311a49f431f358e0fa872601106
Mansi Gupta <gupta.mansi2910@gmail.com>
2025-05-25 16:54:03 +0530

    Update practical1.cpp

--- practical1.cpp
+++ practical1.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <set>
+#include <cmath>  // for pow()
+#include <cstdlib> // for atoi()
 using namespace std;
 
-double seriesSum(int n) {
-    double sum = 0;
-    for (int i = 1; i <= n; i++) {
-        double term = 1.0 / pow(i, 1);
-        if (i % 2 == 0) {
-            sum -= term;
-        } else {
-            sum += term;
-        }
-    }
-    return sum;
-}
-int main() {
+int main(int argc, char* argv[]) {
     int n;
-    cout << "Enter the number of terms: ";
-    cin >> n;
-    cout "Sum of series: " << seriesSum(n) << endl;
+
+    // If user passed n as command line argument
+    if (argc > 1) {
+        n = atoi(argv[1]); // convert argument to int
+    } else {
+        // Prompt user to enter n
+        cout << "Enter the number of terms (n): ";
+        cin >> n;
+    }
+
+    double sum = 0.0;
+
+    for (int i = 1; i <= n; i++) {
+        sum += 1.0 / pow(i, i); // i^i
+    }
+
+    cout << "Sum of the series is: " << sum << endl;
+
     return 0;
 }
