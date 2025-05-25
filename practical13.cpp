b178b174fdf7a1b4c03d9cdd5416df5faf1bebc8
Mansi Gupta <gupta.mansi2910@gmail.com>
2025-05-25 17:21:27 +0530

    Create practical13.cpp

---
+++ practical13.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void removeWhitespace(string inputFile, string outputFile) {
+    ifstream in(inputFile);
+    ofstream out(outputFile);
+    if (!in || !out) {
+        cout << "Error opening files!" << endl;
+        return;
+    }
+    char ch;
+    while (in.get(ch)) {
+        if (!isspace(ch))
+            out.put(ch);
+    }
+    cout << "File copied successfully without whitespaces.\n";
+    in.close();
+    out.close();
+}
+
+int main() {
+    string inputFile = "input.txt";
+    string outputFile = "output.txt";
+    removeWhitespace(inputFile, outputFile);
+    return 0;
+}
